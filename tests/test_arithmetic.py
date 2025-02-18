e47257edfd3a953f7b0100f4a49c377074d4b831
Felipe Montana <f.montana-gonzalez@sheffield.ac.uk>
2025-02-18 10:44:29 +0000

    Merging main branch

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -66,6 +66,7 @@ def test_divide(x: int | float, y: int | float, expected: int | float) -> None:
     assert arithmetic.divide(x, y) == pytest.approx(expected)
 
 
+
 @pytest.mark.parametrize(
     ("x", "target"),
     [
@@ -78,3 +79,9 @@ def test_divide(x: int | float, y: int | float, expected: int | float) -> None:
 def test_square_root(x: int | float, target: int | float) -> None:
     """Test the square_root() function."""
     assert pytest.approx(arithmetic.square_root(x), target)
+
+def test_divide_zero_division_exception() -> None:
+    """Test that a ZeroDivisionError is raised by the divide() function."""
+    with pytest.raises(ZeroDivisionError):
+        arithmetic.divide(2, 0)
+
